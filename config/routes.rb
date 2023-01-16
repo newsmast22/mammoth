ffe942c90b1f6736236d307e82ad327e21324c12
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2023-01-16 14:06:42 +0630

    Typo fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Mammoth::Engine.routes.draw do
 
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
-      post 'egister_with_phone' => "user_sessions#register_with_phone", as: "register_with_phone"
+      post 'register_with_phone' => "user_sessions#register_with_phone", as: "register_with_phone"
       put '/api/v1/verify_otp' => "user_sessions#verify_otp", as: "verify_otp"
 
       resources :communities
