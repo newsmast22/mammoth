b28bfd4ef67e146f7d9d539fbae31bb8aa34ef67
minkhantkyaw35 <45383188+minkhantkyaw35@users.noreply.github.com>
2023-01-26 16:45:33 +0630

    update user suggestion response

--- app/controllers/mammoth/api/v1/users_controller.rb
+++ app/controllers/mammoth/api/v1/users_controller.rb
@@ -6,7 +6,17 @@ module Mammoth::Api::V1
     def suggestion
       @user  = Mammoth::User.find(current_user.id)
       @users = Mammoth::User.joins(:user_communities).where.not(id: @user.id).where(user_communities: {community_id: @user.communities.ids}).distinct
-      render json: {data: @users}
+
+      data   = []
+      @users.each do |user|
+        data << {
+          account_id: user.account_id,
+          user_id: user.id,
+          display_name: user.account.display_name.presence || user.account.username,
+          email: user.email
+        }
+      end
+      render json: {data: data}
     end
 
   end
