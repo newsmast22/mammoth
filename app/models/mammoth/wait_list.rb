593b6a16be1a1f92ffdef6a2723fd06c430a7c87
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2023-05-11 15:55:56 +0630

    Add inverse_of logic

--- app/models/mammoth/wait_list.rb
+++ app/models/mammoth/wait_list.rb
@@ -1,6 +1,7 @@
 module Mammoth
   class WaitList < ApplicationRecord
     self.table_name = 'mammoth_wait_lists'
+    has_one :user, inverse_of: :wait_list
     belongs_to :contributor_role, class_name: "Mammoth::ContributorRole",  optional: true
   end
 end
