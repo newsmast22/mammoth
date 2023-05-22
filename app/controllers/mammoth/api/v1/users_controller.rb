10f066d2567abda11d7474e4c42c0a5dc805380f
minkhantkyaw35 <45383188+minkhantkyaw35@users.noreply.github.com>
2023-05-22 21:06:37 +0630

    fix: user register community modified

--- app/controllers/mammoth/api/v1/users_controller.rb
+++ app/controllers/mammoth/api/v1/users_controller.rb
@@ -182,6 +182,11 @@ module Mammoth::Api::V1
 
         @user.step = @account.country.present? ? "communities" : "country"
         @user.save(validate: false)
+
+        #when user register if primary is 
+        if @user.step == "communities"
+          Mammoth::UserCommunity.where(user_id: current_user.id).destroy_all
+        end
       end
       render json: @account, serializer: Mammoth::CredentialAccountSerializer
     end
