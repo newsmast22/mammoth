9451c6ea96fa9d437313309ab4f01f6f54e8005d
minkhantkyaw35 <45383188+minkhantkyaw35@users.noreply.github.com>
2023-06-09 13:26:36 +0630

    feat: community feed counts

--- app/models/mammoth/community_feed.rb
+++ app/models/mammoth/community_feed.rb
@@ -5,6 +5,30 @@ module Mammoth
     belongs_to :account
     has_many :statuses, class_name: "Mammoth::Status", inverse_of: :community_feed
 
+    scope :feeds_for_admin, -> (community_id) { joins("
+      LEFT JOIN statuses ON statuses.community_feed_id = mammoth_community_feeds.id"
+      )
+      .select("mammoth_community_feeds.*,COUNT(statuses.id) as feed_counts"
+      )
+      .where("mammoth_community_feeds.community_id = :community_id AND mammoth_community_feeds.deleted_at IS NULL",
+        community_id: community_id, 
+       )
+      .order("mammoth_community_feeds.name ASC")
+      .group("mammoth_community_feeds.id")
+    }
+      
+    scope :feeds_for_rss_account, ->(community_id,account_id) { joins("
+      LEFT JOIN statuses ON statuses.community_feed_id = mammoth_community_feeds.id"
+      )
+      .select("mammoth_community_feeds.*,COUNT(statuses.id) as feed_counts"
+      )
+      .where("mammoth_community_feeds.community_id = :community_id AND mammoth_community_feeds.account_id = :account_id AND mammoth_community_feeds.deleted_at IS NULL",
+        community_id: community_id, account_id: account_id, 
+       )
+      .order("mammoth_community_feeds.name ASC")
+      .group("mammoth_community_feeds.id")
+    }
+
     after_save :invoke_rss_worker
 
     private
