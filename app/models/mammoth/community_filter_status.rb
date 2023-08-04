a8b79b183f827708f60cc8b381b562f82a5e768f
minkhantkyaw35 <45383188+minkhantkyaw35@users.noreply.github.com>
2023-08-04 16:33:48 +0630

    fix: create/update status & filter keyword

--- app/models/mammoth/community_filter_status.rb
+++ app/models/mammoth/community_filter_status.rb
@@ -2,5 +2,10 @@ module Mammoth
   class CommunityFilterStatus < ApplicationRecord
     belongs_to :community_filter_keyword, class_name: "Mammoth::CommunityFilterKeyword"
     belongs_to :status, class_name: "Mammoth::Status"
+
+    def delete_filtered_statuses(status_id)
+      self.class.where(status_id: status_id).destroy_all
+    end
+
   end
 end
