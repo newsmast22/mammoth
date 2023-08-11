ae828c4d8706f2cff42db88c83046741d1305690
minkhantkyaw35 <45383188+minkhantkyaw35@users.noreply.github.com>
2023-08-11 11:11:22 +0630

    fix: community filter keyword destroy & post count

--- app/serializers/mammoth/community_filter_keyword_serializer.rb
+++ app/serializers/mammoth/community_filter_keyword_serializer.rb
@@ -2,6 +2,7 @@ class Mammoth::CommunityFilterKeywordSerializer < ActiveModel::Serializer
   attributes :id,:account_id,:community_id,:keyword,:created_at,:post_count
 
   def post_count
-    0
+    object.community_filter_statuses.size
   end
+  
 end
