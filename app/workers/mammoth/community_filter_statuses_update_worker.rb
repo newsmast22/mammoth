e66c84992eefb04fc159d4632ca14e3389ee4a97
minkhantkyaw35 <45383188+minkhantkyaw35@users.noreply.github.com>
2023-09-01 17:33:12 +0630

    fix: comment filter keyword worker

--- app/workers/mammoth/community_filter_statuses_update_worker.rb
+++ app/workers/mammoth/community_filter_statuses_update_worker.rb
@@ -1,17 +1,16 @@
-# module Mammoth
-#   class CommunityFilterStatusesUpdateWorker
-#     include Sidekiq::Worker
+module Mammoth
+  class CommunityFilterStatusesUpdateWorker
+    include Sidekiq::Worker
 
-#     sidekiq_options queue: 'custom_keyword_filter', retry: true, dead: true
+    sidekiq_options queue: 'custom_keyword_filter', retry: true, dead: true
 
-#     def perform(params = {})
+    def perform(params = {})
 
-#       Mammoth::CommunityFilterStatus.where(community_filter_keyword_id: params['community_filter_keyword_id'])&.destroy_all
+      # Mammoth::CommunityFilterStatus.where(community_filter_keyword_id: params['community_filter_keyword_id'])&.destroy_all
 
-#       Mammoth::CommunityStatus.new.create_statuses_by_batch_size(params['community_id'])
+      # Mammoth::CommunityStatus.new.create_statuses_by_batch_size(params['community_id'])
 
-#     end
+    end
 
-#   end
-# end
-
+  end
+end
