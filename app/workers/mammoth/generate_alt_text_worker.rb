9ba8b286c5d9c10a6007fe55a1ea9086f1d5acd1
mac <mac@macui-MacBookPro.local>
2023-09-13 11:28:33 +0630

    fixed code at generated_alt_text_worker.rb

--- app/workers/mammoth/generate_alt_text_worker.rb
+++ app/workers/mammoth/generate_alt_text_worker.rb
@@ -4,11 +4,11 @@ module Mammoth
       sidekiq_options queue: 'generate_alt_text', retry: false, dead: true
   
         def perform(media_attachment_id)
-    
             @media_attachment = Mammoth::MediaAttachment.find(media_attachment_id)
-    
-            if @media_attachment.can_generate_alt?
-                Mammoth::AfterUploadImageService.call(@media_attachment.id)
+            if @media_attachment.present?
+                if @media_attachment.can_generate_alt? 
+                    Mammoth::AfterUploadImageService.new(media_attachment_id).call
+                end
             end
         end
     end
