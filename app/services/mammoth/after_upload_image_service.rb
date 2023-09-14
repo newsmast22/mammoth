1512c0fec2079aa1f9d4138f5d1bcdf1e85e7673
mac <mac@macui-MacBookPro.local>
2023-09-14 14:34:54 +0630

    fixed alttext_create_image

--- app/services/mammoth/after_upload_image_service.rb
+++ app/services/mammoth/after_upload_image_service.rb
@@ -5,46 +5,50 @@ module Mammoth
 
     def initialize(media_attachment_id)
       @media_attachment = Mammoth::MediaAttachment.find(media_attachment_id)
-      @payload = validate_image_url!
-      @api_service = AltTextAiApiService.new(payload: @payload) 
+      @payload = validate_image_url
+      @api_service = AltTextAiApiService.new(payload: @payload)
     end
 
     def call
-      @media_attachment.can_generate_alt?
-      check_api_usage_limit!
-      create_image_at_alttext_ai!
-      update_image_alt!
+      return unless @media_attachment.can_generate_alt?
+      return unless check_api_usage_limit
+      return unless create_image_at_alttext_ai
+      update_image_alt
+      puts "successfully updated alt text : #{@media_attachment.id}"
+    rescue StandardError => e
+      puts "Error: #{e.message}"
     end
 
     private
 
-    def create_image_at_alttext_ai!
+    def create_image_at_alttext_ai
       @alttext_return = @api_service.create_image
+      return false unless @alttext_return && !@alttext_return.has_errors?
+      true
     end
 
-    def check_api_usage_limit!
+    def check_api_usage_limit
       response_body = @api_service.get_account
-      usage_limit = response_body.usage_limit 
-      usage = response_body.usage 
+      return false unless response_body && !response_body.has_errors?
+      usage_limit = response_body.usage_limit
+      usage = response_body.usage
       usage < usage_limit
     end
 
-    def update_image_alt!
-      @media_attachment = MediaAttachment.find(@media_attachment.id)
-      if @alttext_return.present? && @alttext_return.alt_text.present?
+    def update_image_alt
+      puts "update_image_alt for media_id >> #{@media_attachment.id}"
+      @media_attachment.reload
+      if @alttext_return&.alt_text.present?
         @media_attachment.update_column(:auto_generated_description, @alttext_return.alt_text)
-        puts "successfully update alt text with #{@alttext_return.json}"
-      else 
-        puts "can't not generate form api #{@alttext_return.json}"
+      else
+        puts "Generated alt text is nil: #{@media_attachment}"
       end
     end
 
-    def validate_image_url!
-      return nil if @media_attachment.nil?
+    def validate_image_url
+      return {} if @media_attachment.nil?
       image_url = full_asset_url(@media_attachment.file.url(:small))&.remove('mammoth/')
-      puts "upload image url is : #{image_url}"
-      return { image: { url: image_url } }
+      { image: { url: image_url } }
     end
   end
 end
-
--- app/services/mammoth/alt_text_ai_api_service.rb
+++ app/services/mammoth/alt_text_ai_api_service.rb
@@ -8,15 +8,16 @@ module Mammoth
         @base_url = ENV['ALTTEXT_URL']
         @api_key =  ENV['ALTTEXT_SECRET']
         @payload = @options[:payload] if @options.key?(:payload)
-        puts "base url : #{@base_url}, api_key : #{@api_key}, payload : #{@options}"
       end
   
       def get_account
-        make_get_request('account')
+        respon = make_get_request('account')
+        return respon
       end
   
       def create_image
-        make_post_request('images')
+        respon = make_post_request('images')
+        return respon
       end
   
       def make_get_request(endpoint)
@@ -28,12 +29,11 @@ module Mammoth
   
         begin
           response = http.request(request)
-          resp_body_obj = JSON.parse(response.body, object_class: OpenStruct)
-          puts "alttest.ai response body #{resp_body_obj}"
+          resp_body_obj = Mammoth::AlttextGetAccount.new(JSON.parse(response.body))
+          puts "alttest.ai get account info resp body >> #{resp_body_obj.to_json}"
           return resp_body_obj
         rescue StandardError => e
-          puts "Error making GET request: #{e.message}"
-          return false
+          puts "Error making GET request: #{e.message}" 
         end
       end
   
@@ -44,15 +44,13 @@ module Mammoth
         request = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
         request['X-API-Key'] = @api_key
         request.body = @payload.to_json
-  
         begin
           response = http.request(request)
-          resp_body_obj = JSON.parse(response.body, object_class: OpenStruct)
-          puts "alttest.ai response body #{resp_body_obj}"
+          resp_body_obj = Mammoth::AlttextCreateImage.new(JSON.parse(response.body))
+          puts "alttest.ai create image resp body >> #{resp_body_obj.to_json}"
           return resp_body_obj
         rescue StandardError => e
           puts "Error making POST request: #{e.message}"
-          return false
         end
       end
     end
