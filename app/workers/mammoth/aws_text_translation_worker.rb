aa4fd39fb6f907f9d1a15d7e98e19ab5ee4916f3
Min Khant Kyaw <45383188+minkhantkyaw35@users.noreply.github.com>
2023-09-14 11:36:48 +0630

    logging in AWS translate worker

--- app/workers/mammoth/aws_text_translation_worker.rb
+++ app/workers/mammoth/aws_text_translation_worker.rb
@@ -8,9 +8,12 @@ module Mammoth
       status = Mammoth::Status.where(id: status_id).last
 
       unless status.nil? || status.try(:text).nil? || status.try(:text).blank?
+        puts "====================== AwsTextTranslationWorker ========================"
+        puts "====================== status: #{ status.inspect } ========================"
         aws_lamda_service = Mammoth::AwsLamdaTranslateService.new
         translated_text = aws_lamda_service.translate_text(status.text)
         if translated_text.code == 200
+          puts "====================== translated_text: #{ translated_text.inspect } ========================"
           unless translated_text["body"]["original_language"].nil? || translated_text["body"]["original_language"] == "en"
             status.update_columns(language: translated_text["body"]["original_language"], translated_text: translated_text["body"]["translated_text"])
           end
