0457adcb2e96df0275bafa87e1de56c8c0dceb5c
Min Khant Kyaw <45383188+minkhantkyaw35@users.noreply.github.com>
2023-09-15 15:39:14 +0630

    modify: using translate service separately

--- app/workers/mammoth/aws_text_translation_worker.rb
+++ app/workers/mammoth/aws_text_translation_worker.rb
@@ -1,30 +1,17 @@
 module Mammoth
   class AwsTextTranslationWorker
     include Sidekiq::Worker
-    sidekiq_options queue: 'translate_text', retry: false, dead: true
+    sidekiq_options queue: 'aws_translate__text', retry: false, dead: true
 
     def perform(status_id)
-      max_retries = 3
-      retries = 0
-      @status = nil
+      puts "====================== AwsTextTranslationWorker status_id: #{status_id} ======================"
+      # Fetch status_details by status_id
+      @status = Status.find_by(id: status_id.to_i)
+      puts "==============================  @status: #{@status.inspect} =============================="
 
-      while retries < max_retries
-
-        # Fetch status_details by status_id
-        @status = Status.find(status_id.to_i)
-
-        puts "====================== AwsTextTranslationWorker status_id: #{status_id} | max:3 | retries: #{retries} ========================"
-        unless @status.present? || @status.try(:text).nil? || @status.try(:text).blank?
-          puts "==============================  @status: #{@status.inspect} =============================="
-          call_translate_text_service
-          break
-        end
-
-        retries += 1
-        sleep(1)
-
-      end # While Loop:end
-
+      unless !@status.present? || @status.try(:text).nil? || @status.try(:text).blank?
+        call_translate_text_service
+      end
     end
 
     private 
@@ -38,6 +25,7 @@ module Mammoth
         unless translated_text["body"]["original_language"].nil? || translated_text["body"]["original_language"] == "en"
           @status.update_columns(language: translated_text["body"]["original_language"], translated_text: translated_text["body"]["translated_text"])
           puts "====================== (after tranlate) status_id: #{ @status.id }  |  translated_text: #{ @status.try(:translated_text) } ========================"
+          return true if @status
         end
       end
     end
