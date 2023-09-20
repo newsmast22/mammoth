a1c384fdf6d6e81a97d41dff05f465cc0a685499
Min Khant Kyaw <45383188+minkhantkyaw35@users.noreply.github.com>
2023-09-20 11:21:51 +0630

    modify: generate alt only end-users

--- app/models/mammoth/media_attachment.rb
+++ app/models/mammoth/media_attachment.rb
@@ -1,29 +1,37 @@
 module Mammoth
-    class MediaAttachment < MediaAttachment
-        IMAGE_ALLOW_TYPES = %w(image/jpeg image/png image/gif image/webp image/bmp).freeze
+  class MediaAttachment < MediaAttachment
+    IMAGE_ALLOW_TYPES = %w(image/jpeg image/png image/gif image/webp image/bmp).freeze
 
-        def can_generate_alt?
-            is_valid_content_type? && check_file_size? && generate_alt_text? && check_user_desc?
-        end
-        
-        def check_user_desc?
-            flag = !self.description.present? && !self.auto_generated_description.present?
-            puts "description is already exists" if !flag
-            return flag
-        end
-
-        def generate_alt_text?
-            ENV['GENERATE_ALT_TEXT'] ||= false
-        end
-
-        def is_valid_content_type?
-            flag = IMAGE_ALLOW_TYPES.include?(self.file_content_type)
-            puts "invalid content_type is : #{self.file_content_type}" if !flag
-            return flag
-        end
-
-        def check_file_size?
-            self.file_file_size <= 10.megabytes
-        end
+    def can_generate_alt?
+      is_valid_content_type? && check_file_size? && generate_alt_text? && check_user_desc? && is_end_user_upload?
     end
+    
+    def check_user_desc?
+      flag = !self.description.present? && !self.auto_generated_description.present?
+      puts "description is already exists" if !flag
+      return flag
+    end
+
+    def generate_alt_text?
+      ENV['GENERATE_ALT_TEXT'] ||= false
+    end
+
+    def is_valid_content_type?
+      flag = IMAGE_ALLOW_TYPES.include?(self.file_content_type)
+      puts "invalid content_type is : #{self.file_content_type}" if !flag
+      return flag
+    end
+
+    def check_file_size?
+      self.file_file_size <= 10.megabytes
+    end
+
+    def is_end_user_upload?
+      if self.account.domain.nil?
+        return self.account.user.role.name === "" || self.account.user.role.name.empty? 
+      end
+      return false
+    end
+
+  end
 end
