d354e7622e2ec1008015091da1cdb469785ec597
Min Maung Hein <minmaunghein1999@gmail.com>
2023-10-03 16:10:58 +0630

    added tag

--- app/models/mammoth/tag.rb
+++ app/models/mammoth/tag.rb
@@ -1,11 +1,12 @@
 module Mammoth
   class Tag < Tag
     self.table_name = 'tags'
+
     has_many :status_tags, class_name: "Mammoth::StatusTag", foreign_key: "tag_id"
-    has_many :statuses, through: :status_tags
+    has_and_belongs_to_many :statuses, through: :status_tags
 
     has_many :tag_followed, class_name: "Mammoth::TagFollow", foreign_key: "tag_id"
-    has_many :accounts, through: :tag_followed
+    has_and_belongs_to_many :accounts, through: :tag_followed
 
     scope :filter_with_words, ->(words) { where("LOWER(tags.name) like '%#{words}%' OR LOWER(tags.display_name) like '%#{words}%' ") }
 
