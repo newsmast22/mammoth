248baa80a8f6ee013558d0bb4f6b59aa91a5236d
B0V0 <sithubo.stb97@gmail.com>
2023-11-15 09:27:59 +0630

    [fix] get participants count and list

---
+++ app/services/mammoth/community_participants_calculating_service.rb
@@ -0,0 +1,27 @@
+module Mammoth
+  class CommunityParticipantsCalculatingService < BaseService
+
+    def call
+      community_participants_calculator!
+    end
+
+    private
+
+    def community_participants_calculator!
+      Mammoth::Community.all.each do |community|
+        account_data = Rails.cache.fetch("#{community.slug}-participants", expires_in: 1.hour) do
+          Account.select('accounts.*')
+            .joins('LEFT JOIN statuses ON accounts.id = statuses.account_id')
+            .joins('LEFT JOIN mammoth_communities_statuses ON mammoth_communities_statuses.status_id = statuses.id')
+            .joins('LEFT JOIN mammoth_communities ON mammoth_communities.id = mammoth_communities_statuses.community_id')
+            .where.not(domain: nil)
+            .where('mammoth_communities.id = ?', community.id)
+            .group('accounts.id')
+            .order("accounts.id desc")
+        end
+        community.update(community_admin_followed_count: account_data.length) if account_data.present?
+      end
+    end
+    
+  end
+end
