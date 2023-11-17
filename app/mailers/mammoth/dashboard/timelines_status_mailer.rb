2d78bebe8ad0a5fc3b381c74f4b34723d73ab122
B0V0 <sithubo.stb97@gmail.com>
2023-11-17 13:32:12 +0630

    [fix] timelines status notic sending mail logic

--- app/mailers/mammoth/dashboard/timelines_status_mailer.rb
+++ app/mailers/mammoth/dashboard/timelines_status_mailer.rb
@@ -6,10 +6,10 @@ module Mammoth
       @max_active_seconds = max_active_seconds
       @last_status_posted_datetime = last_status_posted_datetime
 
-      to_emails = ENV['TIMELINES_NOTICE_RECIPIENTS'].split(',')
-      to_emails.each do |to_email|
-        mail(to: to_email, subject: "[#{environment}] END POINT #{endpoint_name} is not in operational")
-      end
+      emails = ENV['TIMELINES_NOTICE_RECIPIENTS'].split(',')
+      to_email = emails.first
+      cc_emails = emails[1..-1]
+      mail(to: to_email, cc: cc_emails, subject: "[#{environment}] END POINT #{endpoint_name} is not in operational")
     end
   end
 end
