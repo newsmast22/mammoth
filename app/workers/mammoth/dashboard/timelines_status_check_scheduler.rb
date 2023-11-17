683fd3a5b0e9c41c6fe8063b343298e7ba773a59
B0V0 <sithubo.stb97@gmail.com>
2023-11-17 14:48:22 +0630

    [add] flag to control timelines status

--- app/workers/mammoth/dashboard/timelines_status_check_scheduler.rb
+++ app/workers/mammoth/dashboard/timelines_status_check_scheduler.rb
@@ -11,15 +11,17 @@ module Mammoth
     MAX_PULL_SIZE = 1
     
     def perform
-      return if Sidekiq::Queue.new('timelines_status_check_scheduler').size >= MAX_PULL_SIZE
+      if ENV['TIMELINES_STATUS_CHECKING_ENABLED'] == "true"
+        return if Sidekiq::Queue.new('timelines_status_check_scheduler').size >= MAX_PULL_SIZE
 
-      workers = Sidekiq::Workers.new
-      if workers.count > 0
-        workers.each do |process_id, thread_id, worker|
-          return if worker['queue'] == 'timelines_status_check_scheduler'
+        workers = Sidekiq::Workers.new
+        if workers.count > 0
+          workers.each do |process_id, thread_id, worker|
+            return if worker['queue'] == 'timelines_status_check_scheduler'
+          end
         end
+        Mammoth::Dashboard::TimelinesStatusCheckService.new.call()
       end
-      Mammoth::Dashboard::TimelinesStatusCheckService.new.call()
     end
   end
 end
