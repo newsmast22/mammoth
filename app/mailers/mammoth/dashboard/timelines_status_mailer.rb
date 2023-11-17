d94e3a39c00e7c58ec3df61a78e9262733a1112e
B0V0 <sithubo.stb97@gmail.com>
2023-11-17 11:28:43 +0630

    [fix] timelines status notice send to multiple emails address

--- app/mailers/mammoth/dashboard/timelines_status_mailer.rb
+++ app/mailers/mammoth/dashboard/timelines_status_mailer.rb
@@ -1,11 +1,15 @@
 module Mammoth
   class Dashboard::TimelinesStatusMailer < ApplicationMailer
-    def alarm_email(environment, endpoint_name, max_active_seconds, last_status_posted_datetime, to_email)
+    def alarm_email(environment, endpoint_name, max_active_seconds, last_status_posted_datetime)
       @environment = environment
       @endpoint_name = endpoint_name
       @max_active_seconds = max_active_seconds
       @last_status_posted_datetime = last_status_posted_datetime
-      mail(to: to_email, subject: "[#{environment}] END POINT #{endpoint_name} is not in operational")
+
+      to_emails = ENV['TIMELINES_NOTICE_RECIPIENTS'].split(',')
+      to_emails.each do |to_email|
+        mail(to: to_email, subject: "[#{environment}] END POINT #{endpoint_name} is not in operational")
+      end
     end
   end
 end
