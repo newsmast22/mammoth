18cc46ee4daf3c82127b4f7440b43b8178938b81
Min Maung Hein <minmaunghein1999@gmail.com>
2023-11-22 10:10:15 +0630

    fixed community_filter_status.rb

--- app/models/mammoth/community_filter_status.rb
+++ app/models/mammoth/community_filter_status.rb
@@ -1,6 +1,13 @@
 module Mammoth
   class CommunityFilterStatus < ApplicationRecord
     belongs_to :community_filter_keyword, class_name: "Mammoth::CommunityFilterKeyword"
-    belongs_to :status, class_name: "Mammoth::Status"
+    belongs_to :status, class_name: "Mammoth::Status" 
+    after_create :unpush_form_cache
+
+    private 
+
+    def unpush_form_cache
+      DistributionWorker.perform_async(self.status_id)
+    end
   end
 end
