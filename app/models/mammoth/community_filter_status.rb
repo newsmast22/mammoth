f80e18a47aab63ecdf239e1687e9a9a4b48cd778
Min Maung Hein <minmaunghein1999@gmail.com>
2023-11-24 14:01:56 +0630

    added after_create :unpush form cache

--- app/models/mammoth/community_filter_status.rb
+++ app/models/mammoth/community_filter_status.rb
@@ -2,7 +2,9 @@ module Mammoth
   class CommunityFilterStatus < ApplicationRecord
     belongs_to :community_filter_keyword, class_name: "Mammoth::CommunityFilterKeyword"
     belongs_to :status, class_name: "Mammoth::Status" 
+
     after_create :unpush_form_cache
+    after_update :unpush_form_cache
 
     private 
 
