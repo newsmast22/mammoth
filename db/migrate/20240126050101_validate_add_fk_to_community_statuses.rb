253c8da4b79223cd88d48d2bd9303f438b1ac29b
Min Khant Kyaw <45383188+minkhantkyaw35@users.noreply.github.com>
2024-01-29 14:25:21 +0630

    modify:filter statuses

--- db/migrate/20240126050101_validate_add_fk_to_community_statuses.rb
+++ db/migrate/20240126050101_validate_add_fk_to_community_statuses.rb
@@ -1,5 +1,5 @@
 class ValidateAddFkToCommunityStatuses < ActiveRecord::Migration[6.1]
   def change
-    validate_foreign_key :mammoth_community_filter_statuses, :statuses
+    validate_foreign_key :mammoth_communities_statuses, :statuses
   end
 end
