df053e5f13515467eaa6e2286d65ac988ddf142b
Min Khant Kyaw <45383188+minkhantkyaw35@users.noreply.github.com>
2024-02-17 21:26:54 +0630

    fix:for you timeline boost

--- app/workers/mammoth/boost_community_bot_worker.rb
+++ app/workers/mammoth/boost_community_bot_worker.rb
@@ -4,24 +4,72 @@ module Mammoth
     include FormattingHelper
     sidekiq_options queue: 'custom_bot_boosting', retry: false, dead: true
     
-    def perform(status_id, community_bot_account)
-      post_url = get_post_url(status_id)
-      bot_lamda_service = Mammoth::BoostLamdaCommunityBotService.new
+    def perform(community_id, status_id)      
+      return false unless ENV['BOOST_COMMUNITY_BOT_ENABLED'] == 'true' && ENV['LOCAL_DOMAIN'] == "newsmast.social"
 
-      boost_status = bot_lamda_service.boost_status(community_bot_account, status_id, post_url)
-      if boost_status["statusCode"] == 200
-        return true 
+      @status = Mammoth::Status.find(status_id)
+
+      if community_id.nil?
+        boost_for_all_community
+      else
+        boost_by_community_bot(community_id)
       end
-      false
+
     end
 
     private
 
-    def get_post_url(status_id)
-      status = Status.find(status_id)
-      username = status.account.pretty_acct
-      url = "https://newsmast.social/@#{username}/#{status_id}"
-    end
+      def boost_for_all_community
+        # Looping community to fetch followed accounts by community admin
+        @status.get_admins_from_follow.each do |community_admin|
+          communities = Mammoth::Account.find(community_admin.id).get_owned_communities
+          # Looping community
+          communities.each do |community|
+            boost_by_community_bot(community.id)
+          end
+        end
+      end
+
+      def boost_by_community_bot(community_id)
+        community_bot_account = get_community_bot_account(community_id)
+        return false if community_bot_account.nil? || @status.banned? || is_blocked_by_admins?(community_id, @status.account_id)
+
+        post_url = get_post_url(@status.id)
+        bot_lamda_service = Mammoth::BoostLamdaCommunityBotService.new
+
+        boost_status = bot_lamda_service.boost_status(community_bot_account, @status.id, post_url)
+        return true if boost_status["statusCode"] == 200
+        false
+      end
+
+      def get_post_url(status_id)
+        status = Status.find(status_id)
+        username = status.account.pretty_acct
+        url = "https://newsmast.social/@#{username}/#{status_id}"
+      end
+
+      def get_community_bot_account(community_id)
+        Mammoth::Community.where(id: community_id).last&.bot_account
+      end
+
+      def is_blocked_by_admins?(community_id, account_id)
+        target_account_ids = (
+                              Block
+                              .where(account_id: Mammoth::Account
+                              .joins(users: :community_admins)
+                              .where(community_admins: { community_id: community_id}, users: { role_id: 4 })
+                              .pluck(:id))
+                              .pluck(:target_account_id) + Mute
+                              .where(account_id: Mammoth::Account
+                              .joins(users: :community_admins)
+                              .where(community_admins: { community_id: community_id}, users: { role_id: 4 })
+                              .pluck(:id))
+                              .pluck(:target_account_id)
+                              ).uniq
+        
+        return true if target_account_ids.include?(account_id.to_i)
+        false
+      end
 
   end 
 end
