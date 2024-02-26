8059e4e756aaa366dc57200990380394886b50b8
Min Khant Kyaw <45383188+minkhantkyaw35@users.noreply.github.com>
2024-02-26 10:48:04 +0630

    Update users_controller.rb

--- app/controllers/mammoth/api/v1/users_controller.rb
+++ app/controllers/mammoth/api/v1/users_controller.rb
@@ -353,7 +353,7 @@ module Mammoth::Api::V1
     end
 
     def logout
-      #Mammoth::NotificationToken.find_by(account_id: current_account.id, notification_token: params[:notification_token], platform_type: params[:platform_type]).destroy
+      Mammoth::NotificationToken.where(account_id: current_account.id, notification_token: params[:notification_token], platform_type: params[:platform_type]).last.destroy
       Doorkeeper::AccessToken.where(token: doorkeeper_token.token).last.destroy
       render json: {message: 'logout successed'}
     end
