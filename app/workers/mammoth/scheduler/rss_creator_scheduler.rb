e151700c91c262900c9018698ddbdda131113aa6
Min Khant Kyaw <45383188+minkhantkyaw35@users.noreply.github.com>
2024-02-26 20:38:39 +0630

    Update rss_creator_scheduler.rb

--- app/workers/mammoth/scheduler/rss_creator_scheduler.rb
+++ app/workers/mammoth/scheduler/rss_creator_scheduler.rb
@@ -2,8 +2,7 @@
 module Mammoth
   class Scheduler::RSSCreatorScheduler
     include Sidekiq::Worker
-    #sidekiq_options retry: 0, lock: :until_executed, lock_ttl: 1.day.to_i
-    sidekiq_options retry: 0
+    sidekiq_options retry: 0, lock: :until_executed, lock_ttl: 1.day.to_i
 
     def perform
       Mammoth::CommunityFeed.where.not(custom_url: nil).where(deleted_at: nil).order(created_at: :desc).each do |feed|
