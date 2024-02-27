43d3c8d29de8ee2a80d8a24d178b5c0e212fe80a
Min Khant Kyaw <45383188+minkhantkyaw35@users.noreply.github.com>
2024-02-27 14:16:26 +0630

    fix:rss scheduler job

--- app/workers/mammoth/rss_creator_worker.rb
+++ app/workers/mammoth/rss_creator_worker.rb
@@ -1,13 +1,11 @@
 module Mammoth
   class RSSCreatorWorker
     include Sidekiq::Worker
-
     sidekiq_options backtrace: true, retry: 2, dead: true
 
     def perform(params = {})
       is_callback   = params['is_callback'] == true
       
-      # if is_callback
         if url = params['url'] && is_callback
           params = {
             "community_id"   => params['community_id'],
@@ -17,128 +15,8 @@ module Mammoth
           }
 
           Mammoth::RSSCreatorService.new.call(params)
-          # @cid      = params['community_id']
-          # @cfeed_id = params['feed_id']
-          # @account  = Account.find(params['account_id'])
-          # fetch_feed(url)
         end
-      # else
-      #   # scheduler
-      #   Mammoth::CommunityFeed.where.not(custom_url: nil).where(deleted_at: nil).each do |feed|
-      #     @cid      = feed.community_id
-      #     @account  = feed.account
-      #     @cfeed_id = feed.id
-
-      #     fetch_feed(feed.custom_url)
-      #   end
-      # end
     end
-
-    # private
-
-    #   def fetch_feed(url)
-    #     xml  = HTTParty.get(url).body
-    #     feed = Feedjira.parse(xml)
-    #     fallback_image_url = "https://newsmast-assets.s3.eu-west-2.amazonaws.com/default_fallback_resized.png"
-        
-    #     feed.entries.to_a.sort_by(&:published).each do |item|
-    #       link = item.try(:url) || item.try(:enclosure_url)
-    #       if item.published >= 10.days.ago.to_date
-
-    #         next if @account.statuses.find_by(rss_link: link)
-          
-    #         text  = item.title rescue ''
-    #         desc   = item.summary rescue ''
-    #         @image = get_image_url(item, link) || item.image || fallback_image_url
-    #         @community_slug = Mammoth::Community.where(id: @cid).last.slug
-
-    #         # Check status conent duplication
-    #         search_text = generate_comminity_hashtags(text)
-    #         search_text_link = search_text +" "+link
-
-    #         puts "********************** search_text_link: #{search_text_link} <<<<|>>>> is_status_duplicate?(search_text_link): #{is_status_duplicate?(search_text_link)} ************************"
-
-    #         next if is_status_duplicate?(search_text_link)
-
-    #         create_status(text, desc, link)
-    #         create_community_status if @status
-    #         crawl_Link(link) if @status
-
-    #       end
-    #     end
-    #   rescue => e
-    #     Rails.logger.error "#{e}, URL: #{url}"
-    #   end
-
-    #   def create_status(title, desc, link)
-    #     begin
-
-    #       media_attachment_params = {
-    #         file: URI.open(@image)
-    #       } 
-
-    #       media_attachment = @account.media_attachments.create!(media_attachment_params)
-
-    #       @status = PostStatusService.new.call(
-    #         @account,
-    #         text:              generate_comminity_hashtags(title),
-    #         #spoiler_text:      desc,
-    #         spoiler_text:      "",
-    #         rss_link:          link,
-    #         sensitive:         false, 
-    #         is_rss_content:    true,
-    #         community_feed_id: @cfeed_id,
-    #         community_ids:     [@community_slug],
-    #         media_ids:         [media_attachment.id],
-    #         text_count:        title.blank? ? 0 : title.length
-    #       )
-    #     rescue StandardError => e
-    #       puts "RSS Feed Status creation failed! => error: #{e.inspect}"
-    #     end
-    #   end
-
-    #   def crawl_Link(link)
-    #     assign_text = @status.text
-    #     @status.text = "#{assign_text&.strip} #{link}"
-    #     FetchLinkCardService.new.call(@status)
-    #   rescue ActiveRecord::RecordNotFound
-    #     true
-    #   end
-
-    #   def generate_comminity_hashtags(text)
-    #     @community_ids = Mammoth::Community.where(slug: @community_slug).pluck(:id).to_a.uniq
-    #     community_hash_tags = Mammoth::CommunityHashtag.where(community_id: @community_ids, is_incoming: false)
-    #     post = text
-    #     community_hash_tags.each do |community_hash_tag|
-    #       post += " ##{community_hash_tag.hashtag}"
-    #     end
-    #     return text = post
-    #   end
-
-    #   def create_community_status
-    #     begin
-    #       @community_status = Mammoth::CommunityStatus.find_or_create_by(status: @status, community_id: @cid)
-    #     rescue
-    #       puts 'RSS Feed CommunityStatus creation failed!'
-    #     end
-    #   end
-
-    #   def get_image_url(item, link)
-    #     begin
-    #       url = nil
-    #       if link.present?
-    #         meta = LinkThumbnailer.generate(link)
-    #         url  = meta&.images&.first&.src
-    #       end
-    #       url
-    #     rescue 
-    #       url
-    #     end
-    #   end
-
-    #   def is_status_duplicate?(text)
-    #     Status.where(is_rss_content: true, reply: false).where("text LIKE ?", "%#{text}%").limit(1).exists?
-    #   end
 
   end
 end
--- app/workers/mammoth/scheduler/rss_creator_scheduler.rb
+++ app/workers/mammoth/scheduler/rss_creator_scheduler.rb
@@ -5,7 +5,13 @@ module Mammoth
     sidekiq_options retry: 0, lock: :until_executed, lock_ttl: 1.day.to_i
 
     def perform
-      Mammoth::CommunityFeed.where.not(custom_url: nil).where(deleted_at: nil).order(created_at: :desc).each do |feed|
+      # Calculate the time 15 minutes ago
+      fifteen_minutes_ago = Time.now - 15.minutes
+
+      Mammoth::CommunityFeed.where.not(custom_url: nil)
+      .where(deleted_at: nil)
+      .where("created_at < ?", fifteen_minutes_ago)
+      .order(created_at: :desc).each do |feed|
         params = {
           "community_id"   => feed.community_id,
           "feed_id"        => feed.id,
