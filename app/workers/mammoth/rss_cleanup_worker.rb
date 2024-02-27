9a1c2caa7877040eab8503af5de59c787bc764b0
Min Khant Kyaw <45383188+minkhantkyaw35@users.noreply.github.com>
2024-02-27 16:14:01 +0630

    Update rss_cleanup_worker.rb

--- app/workers/mammoth/rss_cleanup_worker.rb
+++ app/workers/mammoth/rss_cleanup_worker.rb
@@ -6,19 +6,33 @@ module Mammoth
 
     def perform
 
-      Mammoth::CommunityFeed.where(deleted_at: nil).find_in_batches(batch_size: 100).each do |community_feeds|
-        community_feeds.each do |community_feed|
-          Status.where(is_rss_content: true, community_feed_id: community_feed.id ).each do |status|
-            if exceeded_del_schedule?(status.created_at, community_feed.del_schedule) && without_actions?(status)
-              Mammoth::CommunityStatus.where(status: status).destroy_all
-              status.destroy
+      # Remove deleted feed's statuses
+      process_community_feeds(Mammoth::CommunityFeed.where.not(deleted_at: nil))
+
+      # Remove scheduled feed's statuses
+      process_community_feeds(Mammoth::CommunityFeed.where(deleted_at: nil))
+      
+    end
+
+    private
+
+      def process_community_feeds(community_feeds)
+        community_feeds.find_in_batches(batch_size: 100).each do |batch|
+          batch.each do |community_feed|
+            Status.where(is_rss_content: true, community_feed_id: community_feed.id).each do |status|
+              if without_actions?(status)
+                delete_status_and_associations(status) 
+
+                # For scheduled feed's status
+                if exceeded_del_schedule?(status.created_at, community_feed.del_schedule) && community_feed.deleted_at.nil?
+                  delete_status_and_associations(status) 
+                end
+
+              end
             end
           end
         end
       end
-    end
-
-    private
 
       def without_actions?(status)
         reblog = Status.find_by(reblog_of_id: status.id)
@@ -30,5 +44,11 @@ module Mammoth
       def exceeded_del_schedule?(status_date, del_schedule_hour)
         del_schedule_hour.hours.ago > status_date
       end
+
+      def delete_status_and_associations(status)
+        Mammoth::CommunityStatus.where(status: status).destroy_all
+        status.destroy
+      end
+
   end
 end 
