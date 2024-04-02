9c0f1bc964999be6627f9ee4eff382789d9d9055
Min Maung Hein <minmaunghein@Mins-Laptop-2.true.true>
2024-04-02 08:42:25 +0700

    added amplifier setting for newsmast

---
+++ app/controllers/mammoth/api/v1/amplifier_settings_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Mammoth::Api::V1::AmplifierSettingsController < Api::BaseController
+    before_action :require_user!
+    before_action :set_setting
+  
+    def show
+      render json: @setting
+    end
+  
+    def update
+      @setting.update!(selected_filters: params[:selected_filters])
+      render json: @setting
+    end
+  
+    private
+  
+    def set_setting
+      @setting = Mammoth::UserTimelineSetting.where(user: current_user).first_or_initialize(user: current_user)
+    end
+  end
+    
