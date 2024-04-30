09b64264c1a224e1dd5073794b68dcb74a1f0f3b
MinMaungHein1999 <minmaunghein1999@gmail.com>
2024-04-30 18:49:35 +0700

    fixed amplifier_settings_controller.rb

--- app/controllers/mammoth/api/v1/amplifier_settings_controller.rb
+++ app/controllers/mammoth/api/v1/amplifier_settings_controller.rb
@@ -1,23 +1,33 @@
 # frozen_string_literal: true
 
 class Mammoth::Api::V1::AmplifierSettingsController < Api::BaseController
-    before_action :require_user!
-    before_action :set_setting
-  
-    def show
-      render json: @setting
-    end
-  
-    def update
-      @setting.update!(selected_filters: params[:selected_filters])
-      current_user.account.update_excluded_and_domains_from_timeline_cache
-      render json: @setting
-    end
-  
-    private
-  
-    def set_setting
-      @setting = Mammoth::UserTimelineSetting.where(user_id: current_user.id).first_or_initialize(user_id: current_user.id)
-    end
+  before_action :require_user!
+  before_action :set_setting, only: [:show, :update] # Set the setting for both show and update actions
+
+  def show
+    render json: @setting
   end
+
+  def update
+    user_id = current_user.id
+    user_timeline_settings = Mammoth::UserTimelineSetting.where(user_id: user_id)
+
+    if user_timeline_settings.count > 1
+      user_timeline_settings.destroy_all
+      @setting = Mammoth::UserTimelineSetting.create(user_id: user_id) 
+    end
+
+    @setting.update!(selected_filters: params[:selected_filters])
+    current_user.account.update_excluded_and_domains_from_timeline_cache
+
+    render json: @setting
+  end
+
+  private
+
+  def set_setting
+    @setting = Mammoth::UserTimelineSetting.find_or_initialize_by(user_id: current_user.id)
+  end
+end
+
     
