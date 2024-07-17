d43d2ae907fc560816a6d91c4d58f6f3034be5eb
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2024-07-17 12:30:04 +0700

    Update user.rb

--- app/models/mammoth/user.rb
+++ app/models/mammoth/user.rb
@@ -3,12 +3,7 @@ module Mammoth
     self.table_name = 'users'
     
     has_and_belongs_to_many :communities, class_name: "Mammoth::Community"
-    has_many :user_communities , class_name: "Mammoth::UserCommunity", dependent: :destroy
-    has_many :community_admins, class_name: "Mammoth::CommunityAdmin", dependent: :destroy
-    has_many :user_timeline_settings, class_name: "Mammoth::UserTimelineSetting", dependent: :destroy
-    has_many :user_community_settings, class_name: "Mammoth::UserCommunitySetting", dependent: :destroy
     belongs_to :wait_list, inverse_of: :user, optional: true
-
 
     scope :filter_with_words, ->(words) { joins(:account).where("LOWER(users.email) like '%#{words}%' OR LOWER(users.phone) like '%#{words}%' OR LOWER(accounts.username) like '%#{words}%' OR LOWER(accounts.display_name) like '%#{words}%'") }
     scope :filter_blocked_accounts,->(account_ids) {where.not(account_id: account_ids)}
