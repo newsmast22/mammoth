a53e0faf2a50a81d7c1a8ca182bde9a68b3c9113
SithuBo <sithubo.stb97@gmail.com>
2024-08-19 14:12:14 +0630

    [add] close_group if the owner is close group

--- app/services/mammoth/boost_lamda_bot_service.rb
+++ app/services/mammoth/boost_lamda_bot_service.rb
@@ -1,17 +1,26 @@
 module Mammoth
-  class BoostLamdaBotService 
+  class BoostLamdaBotService
   require 'httparty'
-  
-    def initialize  
+
+    def initialize
         @base_url = ENV['BOOST_BOT_URL']
         @api_key = ENV['BOOST_BOT_API_KEY']
     end
-    
+
     def boost_status(status_id)
-      res = HTTParty.post(@base_url, 
+      status = Status.find_by(id: status_id)
+      close_group = ''
+      if status
+        owner_email = status.account.user.email
+        if owner_email == ENV['PRIVATE_COMMUNITY_ACCOUNT_EMAIL']
+          close_group = "@#{ENV['PRIVATE_COMMUNITY_ACCOUNT_EMAIL']}"
+        end
+      end
+      res = HTTParty.post(@base_url,
         :body => {
           "body": {
-              "post_id": status_id.to_s
+              "post_id": status_id.to_s,
+              "close_group": close_group
           }
         }.to_json,
         :headers => {'Content-Type' => 'application/json',
@@ -19,6 +28,6 @@ module Mammoth
                     }
       )
     end
-  
+
   end
 end
