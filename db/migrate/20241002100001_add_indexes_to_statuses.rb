fa24b653538ee2e23af1f06e32f5bbb6522e8526
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2024-10-03 16:18:39 +0630

    Update 20241002100001_add_indexes_to_statuses.rb

--- db/migrate/20241002100001_add_indexes_to_statuses.rb
+++ db/migrate/20241002100001_add_indexes_to_statuses.rb
@@ -1,29 +1,29 @@
-class AddIndexesToStatuses < ActiveRecord::Migration[6.1]
+class AddIndexesToStatuses < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
 
   def change
-    execute('SET lock_timeout TO \'10s\'')
-
     safety_assured do
+      execute('SET lock_timeout TO \'10s\'')
+
       add_index :statuses, :is_rss_content, algorithm: :concurrently
       add_index :statuses, :reply, algorithm: :concurrently
       add_index :statuses, :created_at, algorithm: :concurrently
       add_index :statuses, [:is_rss_content, :reply, :created_at], algorithm: :concurrently
-    end
 
-    execute('RESET lock_timeout')
+      execute('RESET lock_timeout')
+    end
   end
 
   def down
-    execute('SET lock_timeout TO \'10s\'')
-
     safety_assured do
+      execute('SET lock_timeout TO \'10s\'')
+
       remove_index :statuses, :is_rss_content
       remove_index :statuses, :reply
       remove_index :statuses, :created_at
       remove_index :statuses, column: [:is_rss_content, :reply, :created_at]
-    end
 
-    execute('RESET lock_timeout')
+      execute('RESET lock_timeout')
+    end
   end
 end
