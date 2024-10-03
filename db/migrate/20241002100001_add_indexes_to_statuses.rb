a222edfb09f3d4e7758caa516ab16809d48a7864
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2024-10-03 15:33:16 +0630

    Update 20241002100001_add_indexes_to_statuses.rb

--- db/migrate/20241002100001_add_indexes_to_statuses.rb
+++ db/migrate/20241002100001_add_indexes_to_statuses.rb
@@ -4,10 +4,12 @@ class AddIndexesToStatuses < ActiveRecord::Migration[6.1]
   def change
     execute('SET lock_timeout TO \'10s\'')
 
-    add_index :statuses, :is_rss_content, algorithm: :concurrently
-    add_index :statuses, :reply, algorithm: :concurrently
-    add_index :statuses, :created_at, algorithm: :concurrently
-    add_index :statuses, [:is_rss_content, :reply, :created_at], algorithm: :concurrently
+    safety_assured do
+      add_index :statuses, :is_rss_content, algorithm: :concurrently
+      add_index :statuses, :reply, algorithm: :concurrently
+      add_index :statuses, :created_at, algorithm: :concurrently
+      add_index :statuses, [:is_rss_content, :reply, :created_at], algorithm: :concurrently
+    end
 
     execute('RESET lock_timeout')
   end
@@ -15,10 +17,12 @@ class AddIndexesToStatuses < ActiveRecord::Migration[6.1]
   def down
     execute('SET lock_timeout TO \'10s\'')
 
-    remove_index :statuses, :is_rss_content
-    remove_index :statuses, :reply
-    remove_index :statuses, :created_at
-    remove_index :statuses, column: [:is_rss_content, :reply, :created_at]
+    safety_assured do
+      remove_index :statuses, :is_rss_content
+      remove_index :statuses, :reply
+      remove_index :statuses, :created_at
+      remove_index :statuses, column: [:is_rss_content, :reply, :created_at]
+    end
 
     execute('RESET lock_timeout')
   end
