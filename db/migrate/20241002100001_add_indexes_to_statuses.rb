638ef4883e125891bf60f0a1016323c7ca83ebf0
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2024-10-09 17:30:25 +0630

    Update 20241002100001_add_indexes_to_statuses.rb

--- db/migrate/20241002100001_add_indexes_to_statuses.rb
+++ db/migrate/20241002100001_add_indexes_to_statuses.rb
@@ -5,10 +5,10 @@ class AddIndexesToStatuses < ActiveRecord::Migration[7.0]
     safety_assured do
       execute('SET lock_timeout TO \'10s\'')
 
-      add_index :statuses, :is_rss_content, algorithm: :concurrently
-      add_index :statuses, :reply, algorithm: :concurrently
-      add_index :statuses, :created_at, algorithm: :concurrently
-      add_index :statuses, [:is_rss_content, :reply, :created_at], algorithm: :concurrently
+      add_index_if_not_exists :statuses, :is_rss_content, algorithm: :concurrently
+      add_index_if_not_exists :statuses, :reply, algorithm: :concurrently
+      add_index_if_not_exists :statuses, :created_at, algorithm: :concurrently
+      add_index_if_not_exists :statuses, [:is_rss_content, :reply, :created_at], algorithm: :concurrently
 
       execute('RESET lock_timeout')
     end
@@ -18,12 +18,26 @@ class AddIndexesToStatuses < ActiveRecord::Migration[7.0]
     safety_assured do
       execute('SET lock_timeout TO \'10s\'')
 
-      remove_index :statuses, :is_rss_content
-      remove_index :statuses, :reply
-      remove_index :statuses, :created_at
-      remove_index :statuses, column: [:is_rss_content, :reply, :created_at]
+      remove_index_if_exists :statuses, :is_rss_content
+      remove_index_if_exists :statuses, :reply
+      remove_index_if_exists :statuses, :created_at
+      remove_index_if_exists :statuses, column: [:is_rss_content, :reply, :created_at]
 
       execute('RESET lock_timeout')
+    end
+  end
+
+  private
+
+  def add_index_if_not_exists(table_name, column_name, options = {})
+    unless index_exists?(table_name, column_name, options)
+      add_index(table_name, column_name, options)
+    end
+  end
+
+  def remove_index_if_exists(table_name, column_name, options = {})
+    if index_exists?(table_name, column_name, options)
+      remove_index(table_name, column_name, options)
     end
   end
 end
